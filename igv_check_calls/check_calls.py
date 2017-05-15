05e6145577d549899fb0065cfb8a6a09468ef59d
mvdbeek <m.vandenbeek@gmail.com>
2017-05-15 18:25:39 +0200

    Make use of next-previous arrow keys

--- igv_check_calls/check_calls.py
+++ igv_check_calls/check_calls.py
@@ -13,29 +13,46 @@ from AppKit import NSScreen
 from .igv import IGV
 
 
+ACTIONS = {121: True,
+           110: False,
+           109: 'maybe',
+           3: 'next',
+           2: 'previous'}
+
+
 class CheckImages(object):
     """Check a series of images, optionally see images in detail in IGV."""
 
     def __init__(self, files, control_igv, output_path='output.tsv'):
-        self.files = OrderedDict()
-        for file in files:
-            self.files[file] = None
+        self.files = [f for f in files]
+        self.result = OrderedDict()
         self.control_igv = control_igv
         self.output_path = output_path
         self.screen_height = NSScreen.mainScreen().frame().size.height
         self.process()
+        self.write()
 
-    def process(self):
-        for i, filename in enumerate(self.files):
-            self.check_image(filename=filename)
+    @property
+    def image_number_to_process(self):
+        return [i for i, filename in enumerate(self.files) if not filename in self.result]
 
+    def process(self, start=0):
+        for i, filename in enumerate(self.files[start:]):
+            total_i = i + start
+            _, action = self.check_image(filename=filename)
+            print(action)
+            if action in {True, False, 'maybe'}:
+                self.result[filename] = action
+            if action == 'previous':
+                return self.process(start=total_i - 1)
+        if len(self.image_number_to_process) > 0:
+            self.process(start=self.image_number_to_process[0])
 
     def check_image(self, filename):
         image = cv2.imread(filename)
         image_name = os.path.basename(filename)
         is_positive = self._check(image, image_name)
         return image_name, is_positive
-
 
     def _check(self, image, image_name):
         cv2.namedWindow(image_name, cv2.WINDOW_NORMAL)
@@ -46,24 +63,17 @@ class CheckImages(object):
             # display the image and wait for a keypress
             cv2.imshow(image_name, small)
             key = cv2.waitKey(1)
+            if key in ACTIONS:
+                cv2.destroyAllWindows()
+                return ACTIONS[key]
 
-            # if the 'y' key is pressed return True
-            if key == ord("y"):
-                cv2.destroyWindow(image_name)
-                print('True')
-                return True
+    def write(self):
+        template = "%s\t%s\n"
+        with open(self.output_path, 'w') as out:
+            for filename in self.result:
+                image_name = "\t".join(os.path.basename(filename).split('_', 3))
+                out.write(template % (image_name, self.result[filename]))
 
-            # if the 'n' key is pressed return False
-            elif key == ord("n"):
-                print('False')
-                cv2.destroyWindow(image_name)
-                return False
-
-            # if the 'm' key is pressed return Maybe
-            elif key == ord("m"):
-                print('Maybe')
-                cv2.destroyWindow(image_name)
-                return 'Maybe'
 
 
 @click.command()
